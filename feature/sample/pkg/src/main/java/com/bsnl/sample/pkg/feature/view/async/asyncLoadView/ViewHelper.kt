4496d66e3aa9c4a29a036a3df49602ddb9715673
李兆行 <lizhaoxing@ebupt.com>
2020-12-30 17:48:35 +0800

    doc# 备注异步Inflate View注意事项

--- feature/sample/pkg/src/main/java/com/bsnl/sample/pkg/feature/view/async/asyncLoadView/ViewHelper.kt
+++ feature/sample/pkg/src/main/java/com/bsnl/sample/pkg/feature/view/async/asyncLoadView/ViewHelper.kt
@@ -12,7 +12,9 @@ import kotlinx.coroutines.launch
 /**
  * @author : LeeZhaoXing
  * @date   : 2020/12/29
- * @desc   :
+ * @note:   1）使用异步 Inflate+全局缓存构建的 View，在使用时需要重新设置 LayoutParams，不然显示上可能不是最终想要的结果；
+ *          2）使用异步 Inflate+全局缓存构建的 View，如果 View 的解析过程中，存在 Theme 相关的，可能会导致 View 构建失败。如原生的 TabLayout，解析时会读取 Theme 中的属性，如果 context 传入的是 Application 且没有设置相关 Theme，就会报错；
+ *          3）使用异步 Inflate+全局缓存构建的 View，需要及时的调用 refreshCurrentActivity 方法，这样尽可能的保持 context 是当前的 activity 实例。在使用 context 的时候，避免直接把 context 强转 activity，而是使用 ViewContext 的 getActivity 方法获取。
  */
 class ViewHelper private constructor() {
     private val mViewPool = SynchronizedPool<View>(10)
@@ -49,7 +51,8 @@ class ViewHelper private constructor() {
     fun asyncPreLoadView(layoutId: Int, listener: IAsyncLoadView? = null) {
         GlobalScope.launch(Dispatchers.IO) {
             if (replaceLooperWithMainThreadQueue(false)) {
-                val view = LayoutInflater.from(viewContext?.getCurrentContext()).inflate(layoutId, null)
+                val view =
+                    LayoutInflater.from(viewContext?.getCurrentContext()).inflate(layoutId, null)
                 replaceLooperWithMainThreadQueue(true)
                 mViewPool.release(layoutId, view)
                 listener?.generatedView(view)
